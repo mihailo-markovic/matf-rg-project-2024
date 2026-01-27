ff780fb87f6f4a3d75a9f055d26915fc443519ef
mihailo-markovic <mihailoo003@gmail.com>
2026-01-27 18:19:08 +0100

    Implemented camera

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -6,12 +6,20 @@
 #define MATF_RG_PROJECT_MAINCONTROLLER_HPP
 #include <engine/core/App.hpp>
 #include <engine/core/Controller.hpp>
+#include <engine/platform/PlatformEventObserver.hpp>
 
 namespace app {
+class MainPlatformEventObserver : public engine::platform::PlatformEventObserver {
+public:
+    void on_mouse_move(engine::platform::MousePosition position) override;
+};
+
 class MainController : public engine::core::Controller {
     void initialize() override;
 
     bool loop() override;
+
+    void update() override;
 
     void begin_draw() override;
 
@@ -20,6 +28,8 @@ class MainController : public engine::core::Controller {
     void end_draw() override;
 
     void draw_Ferdinand();
+
+    void update_camera();
 
 public:
     std::string_view name() const override { return "app::MainController"; }
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -15,7 +15,19 @@
 #include <spdlog/spdlog.h>
 
 namespace app {
-void MainController::initialize() { engine::graphics::OpenGL::enable_depth_testing(); }
+
+void MainPlatformEventObserver::on_mouse_move(engine::platform::MousePosition position) {
+    auto camera = engine::core::Controller::get<engine::graphics::GraphicsController>()->camera();
+
+    camera->rotate_camera(position.dx * 0.5, -position.dy * 0.5);
+
+}
+
+void MainController::initialize() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    platform->register_platform_event_observer(std::make_unique<MainPlatformEventObserver>());
+    engine::graphics::OpenGL::enable_depth_testing();
+}
 
 bool MainController::loop() {
     auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
@@ -39,6 +51,19 @@ void MainController::draw_Ferdinand() {
     shader->set_mat4("model", model);
     ferdinand->draw(shader);
 }
+
+void MainController::update_camera() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    auto camera = graphics->camera();
+    float dt = platform->dt();
+    if (platform->key(engine::platform::KEY_W).is_down()) { camera->move_camera(engine::graphics::Camera::Movement::FORWARD, dt); }
+    if (platform->key(engine::platform::KEY_A).is_down()) { camera->move_camera(engine::graphics::Camera::Movement::LEFT, dt); }
+    if (platform->key(engine::platform::KEY_S).is_down()) { camera->move_camera(engine::graphics::Camera::Movement::BACKWARD, dt); }
+    if (platform->key(engine::platform::KEY_D).is_down()) { camera->move_camera(engine::graphics::Camera::Movement::RIGHT, dt); }
+}
+
+void MainController::update() { update_camera(); }
 
 void MainController::begin_draw() { engine::graphics::OpenGL::clear_buffers(); }
 
